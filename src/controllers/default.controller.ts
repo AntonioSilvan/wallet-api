a3b34552872e41b59048234b346ea689355e6a94
AntonioSilvan <32887600+AntonioSilvan@users.noreply.github.com>
2021-07-02 23:53:59 -0500

    Add example controller

--- src/controllers/default.controller.ts
+++ src/controllers/default.controller.ts
@@ -15,4 +15,13 @@ export class DefaultController{
             message: "Default controller on"
         });
     }
+
+    @route('index')
+    @GET()
+    public start( req:Request, res:Response){
+        res.send({
+            status: true,
+            message: "Start the application"
+        })
+    }
 }
